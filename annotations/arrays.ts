1c6085e6b025083fb6aafbdd52b605f995b079ce
Ravi <samcollins9899@gmail.com>
2022-10-24 22:56:35 +0530

    added array ts objtect

---
+++ annotations/arrays.ts
@@ -0,0 +1,29 @@
+
+
+//Help with inference with extracting values
+const carMakers = ["ford", "toyata", "chevy"]
+const dates = [new Date(), new Date()];
+
+const carr = carMakers[0];
+console.log(carr)
+
+const myCar = carMakers.pop();
+
+//prevent incompatiable types.
+carMakers.push(200);
+
+//help with array functions
+
+carMakers.map (
+    (car: string):string =>{
+    return car.toLocaleUpperCase();
+});
+
+
+//Flexible types
+
+const importandDates: (Date | string) [] = [new Date()];
+
+importandDates.push('10-10-2030');
+importandDates.push(new Date());
+
